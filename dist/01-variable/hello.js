454c928864d24adcfca94da3601a7d4f5d5476c7
Priyaddharshan-S <priyaddharshan@gmail.com>
2026-07-06 12:54:57 +0530

    variable was added

---
+++ dist/01-variable/hello.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+TypeScript offers two ways to work with types:
+
+1. Explicit Typing: You explicitly declare the type of a variable. Use For function parameter and return type, object literals
+
+2. Type Inference: TypeScript automatically determines the type based on the assigned value. Use for variable declaration.
+
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "Priyaddharshan"; // Explict Typing
+let char = "hello"; // Implicit Typing
+//# sourceMappingURL=hello.js.map
---
+++ src/01-variable/hello.ts
@@ -0,0 +1,12 @@
+/*
+TypeScript offers two ways to work with types:
+
+1. Explicit Typing: You explicitly declare the type of a variable. Use For function parameter and return type, object literals
+
+2. Type Inference: TypeScript automatically determines the type based on the assigned value. Use for variable declaration.
+
+*/
+
+let name: string = "Priyaddharshan"; // Explict Typing
+
+let char = "hello"; // Implicit Typing
