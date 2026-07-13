ca12a0222111c63a864db0e8dca6c13273bca5ba
Priyaddharshan-S <priyaddharshan@gmail.com>
2026-07-13 23:57:41 +0530

    Object added

--- src/05-objects/object.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const user = {
-    name: 'leo',
-    age: 32,
-    city: 'ODY'
-};
-console.log(user);
-console.log(user.name);
--- src/05-objects/object.ts
+++
@@ -1,8 +0,0 @@
-const user : {name: string, age: number, city: string} = {
-    name: 'leo',
-    age: 32,
-    city: 'ODY'
-}
-console.log(user);
-console.log(user.name);
-
